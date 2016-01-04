67cf5cdf437060b6597d85fef078a355d933e189
FreddyLFSVM <fredlee590@gmail.com>
2016-01-03 22:43:46 -0800

    After building 5.10 - GCC pass 2. Does not include /tools/libexec/gcc/x86_64-unknown-linux-gnu/5.2.0/cc1, cc1plus, and lto1, which are too big for Git.

---
+++ tools/lib/gcc/x86_64-unknown-linux-gnu/5.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-5.2.0/configure --prefix=/tools --with-local-prefix=/tools --with-native-system-header-dir=/tools/include --enable-languages=c,c++ --disable-libstdcxx-pch --disable-multilib --disable-bootstrap --disable-gomp";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" } };
